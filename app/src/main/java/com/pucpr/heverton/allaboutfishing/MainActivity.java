17758a21d4f2c428fa21dd16506457f6095cf3ec
Heverton <ton1451@hotmail.com>
2019-04-06 21:14:11 -0300

    Second commit... 06/04/2019

--- app/src/main/java/com/pucpr/heverton/allaboutfishing/MainActivity.java
+++ app/src/main/java/com/pucpr/heverton/allaboutfishing/MainActivity.java
@@ -1,5 +1,7 @@
 package com.pucpr.heverton.allaboutfishing;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarLogin();
+            }
+        }, 2000);
+    }
+
+    private void mostrarLogin() {
+        Intent intent = new Intent(MainActivity.this,
+                LoginActivity.class);
+        startActivityForResult(intent,1);
+        finish();
     }
 }
